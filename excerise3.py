df60ecf839ef8f9ac6463cfbadb59362fa56eeda
piyush <PIYUSH72.24@ichat.sp.edu.sg>
2025-04-29 23:26:01 +0800

    This is additoon

---
+++ excerise3.py
@@ -0,0 +1,9 @@
+def display_main_menu():
+    numb=input(int("Enter number of numbers you want to enter"))
+    my_list=[]
+    for i in range(numb):
+      my_list.append(int(numb))
+      print(my_list)
+
+        
+display_main_menu()
