664be4035a85a501c06e8d44d6ad2b62d2684aab
piyush <PIYUSH72.24@ichat.sp.edu.sg>
2025-04-29 15:35:46 +0800

    Hi

---
+++ bmi.py
@@ -0,0 +1,24 @@
+def calculate_bmi(height,weight):
+    print("Height")
+    print("Weight")
+    bmi = weight/(height*height)
+    print("BMI" +str(bmi))
+    return bmi
+
+def classify_bmi(bmi):
+    if(bmi< 18.5):
+        print("Underweight")
+    elif(bmi>=18.5 and bmi<=25):
+        print("Normal Weight")
+    else:
+        print("Overweight,excerise!!!!")
+
+def app():
+    output =calculate_bmi(1,7,60)
+    classify_bmi(output)
+    return
+
+
+calculate_bmi(10,20)
+
+print
