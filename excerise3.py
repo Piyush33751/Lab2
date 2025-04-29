eab122615a47303224401fd6545472e44b89d346
piyush <PIYUSH72.24@ichat.sp.edu.sg>
2025-04-29 23:42:21 +0800

    Ops change

--- excerise3.py
+++ excerise3.py
@@ -1,8 +1,7 @@
-
-
 number = input("The number of numbers you want to input")
 my_list=[]
-for i in range(number):
-    my_list.append(number)
+for i in range(int(number)):
+    numb = input("Input your numbers here")
+    my_list.append(numb)
     print(my_list)
 
